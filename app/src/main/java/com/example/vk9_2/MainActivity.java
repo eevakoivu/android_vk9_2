b3216f70a5a50db1f453801863be4027707d48e2
eevakoivu <eevamaria.koivu@gmail.com>
2022-03-23 13:12:08 +0200

    Initial commit

--- app/src/main/java/com/example/vk9_2/MainActivity.java
+++ app/src/main/java/com/example/vk9_2/MainActivity.java
@@ -1,80 +1,149 @@
 package com.example.vk9_2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Locale;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity{
+    Spinner spinner = null;
+    Button timebutton = null;
+    CalendarView calendarView = null;
+    EditText editText = null;
+    ListView listView = null;
+    int hour, minute;
+    int year, month, day;
+    String[] theaterlist = {"Espoo Omena", "Espoo Sello", "Helsinki Itis", "Helsinki Kinopalatsi",
+            "Helsinki Maxim", "Helsinki Tennispalatsi", "Vantaa Flamingo", "Jyväskylä Fantasia", "Kuopio Scala",
+            "Lahti Kuvapalatsi", "Lappeenranta Strand", "Oulu Plaza", "Pori Promenadi", "Tampere Cine Atlas",
+            "Tampere Plevna", "Turku Kinopalatsi"};
+    String[] movies = {"movie1", "movie2", "movie3"};
 
-    Spinner theaters = null;
-    Context context = null;
+    ArrayAdapter<String> adapter2 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        context = MainActivity.this;
-        theaters = (Spinner) findViewById(R.id.theaters);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        timebutton = (Button) findViewById(R.id.timebutton);
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        editText = (EditText) findViewById(R.id.editText);
+        listView = (ListView) findViewById(R.id.listView);
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
+        spinner();
+        calendarView();
+        listView();
+        editText();
         readXML();
-        /*theaters.setOnItemSelectedListener(this);
 
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, theaterlist); //LISÄÄ TEATTERILISTA!
+
+    }
+
+    public void spinner(){
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, theaterlist);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        theaters.setAdapter(adapter); */
+        spinner.setAdapter(adapter);
+
 
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+    //datepicker??
+    public void calendarView(){
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                year=i;
+                month=i1+1;
+                day=i2;
+                //System.out.println(day+"."+month+"."+year);
+            }
+        });
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+    public void timePicker(View v){
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                hour = i;
+                minute = i1;
+                timebutton.setText(String.format(Locale.getDefault(), "%02d:%02d",hour,minute));
+            }
+        };
 
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
+        timePickerDialog.setTitle("Select Time");
+        timePickerDialog.show();
     }
 
-    public void readXML(){ //pitääkö olla mainclassissa?
-        try {
-            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            String urlString = "https://www.finnkino.fi/xml/TheatreAreas/";
-            Document doc = builder.parse(urlString);
-            doc.getDocumentElement().normalize();
-            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
-
-        } catch(IOException e) {
-            e.printStackTrace();
-        } catch(SAXException e) {
-            e.printStackTrace();
-        } catch(ParserConfigurationException e) {
-            e.printStackTrace();
-        } finally{
-            System.out.println("Done");
-        }
+    //edittext
+    //nimi.setontextchangelastener(new NiMi.OntextchangeListener)
+    public void listView(){
+        adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, movies);
+        listView.setAdapter(adapter2);
+    }
 
 
+    public void editText(){
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                adapter2.getFilter().filter(editable);
+            }
+        });
+    }
+
+    public void readXML(){
+        //kutsutaan mainclass readXML ja saadaan lista elokuvateat.
     }
 }
 
--- app/src/main/java/com/example/vk9_2/Mainclass.java
+++ app/src/main/java/com/example/vk9_2/Mainclass.java
@@ -1,6 +1,9 @@
 package com.example.vk9_2;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
@@ -13,5 +16,40 @@ public class Mainclass {
     //pitää kirjaa kaikista elokuvateattereista
     //käyttöliittymään
 
+    //oliot teattereista (vrt. Bottle) Theaterinfo
+    //lue lista teattereista -> MainActivitiin
+
+    public void readXML() {
+        try {
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            String urlString = "https://www.finnkino.fi/xml/TheatreAreas/";
+            Document doc = builder.parse(urlString);
+            doc.getDocumentElement().normalize();
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getDocumentElement().getElementsByTagName("TheatreArea");
+
+            for(int i=0; i<nList.getLength(); i++){
+                Node node = nList.item(i); //yhden teatterin tiedot
+
+                if(node.getNodeType() == Node.ELEMENT_NODE){
+                    Element element = (Element) node;
+
+                    element.getElementsByTagName("ID").item(0).getTextContent();
+                    element.getElementsByTagName("Name").item(0).getTextContent();
+                }
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Done");
+        }
+    }
 }
 
